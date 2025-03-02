41204a146b0c427a5b2b4582cb223c0b8e1f4d65
B0cka <Bocka1423@yandex.ru>
2025-03-02 21:41:14 +0300

    "Удалены лишние аннотации @Getter и @Setter в User.java"

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
 
 import java.time.LocalDate;
 
 @Data
-@Getter
-@Setter
+
 public class User {
     private long id;
     private String email;
