ead332b43fdad32f8dc86ee2518d3959fb49e9d7
B0cka <Bocka1423@yandex.ru>
2025-03-02 21:20:42 +0300

    for_work_1.1

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,21 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.time.LocalDate;
-
-@Data
-@Getter
-@Setter
 public class User {
-    private int id;
-    private String email;
-    private String login;
-    private String username;
-    private LocalDate birthday;
-
 
 }
 
