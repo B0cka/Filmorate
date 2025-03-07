831c523f7879e6bc5d5248b445818b278db2737e
B0cka <Bocka1423@yandex.ru>
2025-03-07 23:06:30 +0300

    добавлена валидация, убрано дублирование, тесты рассортированы по разным файлам

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -3,34 +3,20 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import org.junit.jupiter.api.Assertions;
 
 import java.time.LocalDate;
 
-public class FilmorateApplicationTests {
-
-    private UserController userController = new UserController();
+class FilmControllerTest {
     private FilmController filmController;
-    private User validUser;
     private Film validFilm;
 
     @BeforeEach
     void setUp() {
-        // Инициализация контроллеров и объектов
-        userController = new UserController();
         filmController = new FilmController();
-
-        validUser = new User();
-        validUser.setEmail("test@example.com");
-        validUser.setLogin("testUser");
-        validUser.setName("Test");
-        validUser.setBirthday(LocalDate.parse("1990-01-01"));
-
         validFilm = new Film();
         validFilm.setName("Inception");
         validFilm.setDescription("A mind-bending thriller.");
@@ -38,37 +24,30 @@ public class FilmorateApplicationTests {
         validFilm.setDuration(148);
     }
 
-
     @Test
     void shouldNotCreateFilmWithTooLongDescription() {
         validFilm.setDescription("A".repeat(201));
-
         ValidationException thrown = Assertions.assertThrows(ValidationException.class, () -> {
             filmController.create(validFilm);
         });
-
-        Assertions.assertEquals("максимальная длина описания — 200 символов", thrown.getMessage());
+        Assertions.assertEquals("Максимальная длина описания — 200 символов", thrown.getMessage());
     }
 
     @Test
     void shouldNotCreateFilmWithReleaseDateBefore1895() {
         validFilm.setReleaseDate(LocalDate.parse("1890-01-01"));
-
         ValidationException thrown = Assertions.assertThrows(ValidationException.class, () -> {
             filmController.create(validFilm);
         });
-
         Assertions.assertEquals("Дата релиза — не раньше 28 декабря 1895 года", thrown.getMessage());
     }
 
     @Test
     void shouldNotCreateFilmWithNegativeDuration() {
-        validFilm.setDuration(-120);
-
+        validFilm.setDuration(-10010);
         ValidationException thrown = Assertions.assertThrows(ValidationException.class, () -> {
             filmController.create(validFilm);
         });
-
-        Assertions.assertEquals("продолжительность фильма должна быть положительным числом", thrown.getMessage());
+        Assertions.assertEquals("Продолжительность фильма должна быть положительным числом", thrown.getMessage());
     }
 }
