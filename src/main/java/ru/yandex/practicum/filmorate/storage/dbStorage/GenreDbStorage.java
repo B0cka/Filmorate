543ae7d97729c217442164c32f7a9d153b716e86
B0cka <Bocka1423@yandex.ru>
2025-04-06 22:55:13 +0300

    Error handling has been moved to where it should be_5

--- src/main/java/ru/yandex/practicum/filmorate/storage/dbStorage/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dbStorage/GenreDbStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.dbStorage;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
